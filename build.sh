7a0df21ef5c638840f8f0b8a9592adb507a39f12
brunobaronims <brunobaronims@gmail.com>
2026-05-26 22:47:49 -0300

    add example program build

--- build.sh
+++ build.sh
@@ -15,12 +15,14 @@ compiler='gcc'
 archiver='ar'
 platform_src=''
 platform_obj=''
+linker_flags=''
 
 if [[ -v cross ]]; then
 	compiler='x86_64-w64-mingw32-gcc'
 	archiver='x86_64-w64-mingw32-ar'
 	platform_src+='src/windows/direct2d.c src/windows/window.c'
 	platform_obj+='direct2d.o'
+	linker_flags+='-ld2d1 -lole32 -luuid'
 fi
 
 flags=''
@@ -29,17 +31,14 @@ if [[ -v debug ]]; then
 fi
 
 echo "compiling static lib"
-
 if ! ${compiler} ${flags} -Iinclude -c src/cava.c ${platform_src}; then
 	echo "compilation failed"
 	exit 1
 fi
-
 if ! ${archiver} rcs libcava.a cava.o window.o ${platform_obj}; then
 	echo "archiving failed"
 	exit 1
 fi
-echo
 
 rm cava.o
 rm window.o
@@ -47,5 +46,14 @@ rm ${platform_obj}
 
 mkdir -p lib
 mv libcava.a lib
+
+if [[ -v debug ]]; then
+	echo "compiling example program"
+
+	if ! ${compiler} ${flags} -Iinclude -Llib example/example.c -o example.exe -lcava ${linker_flags}; then
+		echo "example compilation failed"
+		exit 1
+	fi
+fi
 
 echo "success!"
